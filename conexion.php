4230998f6119ff7933aee87cf96c2076673e1faf
FxPhantomhive <40579571+FxPhantomhive@users.noreply.github.com>
2018-06-30 14:18:32 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,20 @@
+﻿<?php
+	
+	$usuario = "xr753v8p_FxPhant";
+	$password = "Felix.Melendez20!8";
+	$servidor = "108.167.157.231:3306";
+	$basededatos = "xr753v8p_cifco";
+	
+	
+	// creación de la conexión a la base de datos con mysql_connect()
+	$conexion = mysqli_connect( $servidor, $usuario, $password ) or die ("No se ha podido conectar al servidor de Base de datos");
+	
+	// Selección del a base de datos a utilizar
+	$db = mysqli_select_db( $conexion, $basededatos ) or die ( "Upps! Pues va a ser que no se ha podido conectar a la base de datos" );
+	// establecer y realizar consulta. guardamos en variable.
+	if(mysqli_connect_errno()){
+		echo 'Conexion Fallida : ', mysqli_connect_error();
+		exit();
+	}
+	
+?>
